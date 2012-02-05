e7ea1a8fbeb5420f4d01348fe368cfd484037ec0
Jon Oliver <contact@jonathancoliver.com>
2012-02-04 20:52:19 -0500

    add require.js

--- js/ts.init.js
+++ js/ts.init.js
@@ -1,3 +1,17 @@
 // create typeswift namespace where all modules will reside
 var Typeswift = function(){}
 var ts = new Typeswift();
+
+//require necessary components
+require([
+	"js/lib/jquery-1.4.2.min.js",
+	"js/mod/ts.Log.js",
+	"js/mod/ts.String.js",
+	"js/mod/ts.LocalDataProxy.js",
+	"js/mod/ts.StatCounter.js",
+	"js/mod/ts.Timer.js",
+	"js/mod/ts.Test.js",
+	"js/mod/ts.Keyboard.js",
+	"js/mod/ts.PageHandler.js",
+	"js/ts.ui.js"
+]);
